6a891407d325c27be5a0c8ca290fdb4565fdadd9
Sergei Aidinov <incoming34@yandex.ru>
2022-11-05 18:42:35 +0300

    Лучшее из того, что получилось

--- src/main/java/ru/yandex/incoming34/VaadinTrial/ui/MainView.java
+++ src/main/java/ru/yandex/incoming34/VaadinTrial/ui/MainView.java
@@ -1,73 +1,95 @@
 package ru.yandex.incoming34.VaadinTrial.ui;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
-//@Component
 @Route
 public class MainView extends VerticalLayout {
+
+    private static final String RESPOND = "Répondre";
+    private static final String INVITATION = "Conjuguons le verbe: ";
 
     @Autowired
     FrenchLettersUtils frenchLettersUtils;
 
-    List<FrenchLettersUtils.letters> lettersList;
-
-
-
-    Button button = new Button("Hello!");
-
-
-
-   // Button accentButton = new Button(frenchLettersUtils.getSymbol(FrenchLettersUtils.letters.A_ACCENT_GRAVE));
-
-    List<Button> buttonList = new ArrayList<>();
-    TextField nameField = new TextField();
-    HorizontalLayout layout = new HorizontalLayout();
+    TextField answerField = createAnswerField();
+    TextField inviteField = createInviteField();
+    Button respondButton = createRespondButton();
+    VerticalLayout verticalLayout = createVerticalLayout();
+    private final String WIDTH = "250px";
+    private final String LAYOUT_WIDTH = "300px";
 
     @PostConstruct
-    private void initialize(){
-        lettersList = frenchLettersUtils.getAllLetters().stream().collect(Collectors.toList());
+    private void initialize() {
 
-        lettersList.forEach(letter -> {
-           Button nextButton =  new Button(frenchLettersUtils.getSymbol(letter));
-           nextButton.addClickListener(clickEvent ->
-                   nameField.setValue(nameField.getValue() + frenchLettersUtils.getSymbol(letter)));
-           layout.add(nextButton);
+        List<FrenchLettersUtils.letters> lettersList = frenchLettersUtils.getAllLetters().stream().collect(Collectors.toList());
+        Iterator<FrenchLettersUtils.letters> iterator = lettersList.iterator();
+
+        while (iterator.hasNext()) {
+            HorizontalLayout horizontalLayout = new HorizontalLayout();
+            horizontalLayout.setWidth(LAYOUT_WIDTH);
+            for (int i = 0; i < 3; i++) {
+                if (iterator.hasNext()) {
+                    FrenchLettersUtils.letters letter = iterator.next();
+                    Button nextButton = new Button(frenchLettersUtils.getSymbol(letter));
+                    nextButton.addClickListener(clickEvent ->
+                            answerField.setValue(answerField.getValue() + frenchLettersUtils.getSymbol(letter)));
+                    horizontalLayout.add(nextButton);
+                }
+            }
+            verticalLayout.add(horizontalLayout);
+        }
+
+        verticalLayout.add(respondButton);
+        verticalLayout.setVisible(true);
+        add(verticalLayout);
+
+    }
+
+    private TextField createAnswerField() {
+        TextField answerField = new TextField();
+        answerField.setWidth(WIDTH);
+        return answerField;
+    }
+
+    private TextField createInviteField() {
+
+        TextField inviteField = new TextField();
+        inviteField.setWidth(WIDTH);
+        inviteField.setValue(INVITATION);
+        inviteField.setReadOnly(true);
+        inviteField.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        return inviteField;
+    }
+
+    private Button createRespondButton() {
+        Button respondButton = new Button(RESPOND);
+        respondButton.setWidth(WIDTH);
+        respondButton.addClickListener(clickEvent -> {
+            String answer = answerField.getValue();
+            inviteField.setValue(answer);
+
         });
-
-        layout.add(nameField);
+        return respondButton;
     }
 
-
-    public MainView() {
-
-/*
-        button.addClickListener(clickEvent ->
-                add(new Text("Hello, " + nameField.getValue() + "! "))
-        );
-
-        //buttonList.forEach(element -> layout.add(element));
-
-
- */
-
-        layout.setVisible(true);
-        add(layout);
-        setSizeFull();
-
+    private VerticalLayout createVerticalLayout() {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setWidth(LAYOUT_WIDTH);
+        verticalLayout.add(inviteField);
+        verticalLayout.add(answerField);
+        return verticalLayout;
     }
+
 
 }
 
